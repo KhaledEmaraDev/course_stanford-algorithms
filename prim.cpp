5e0ececf2687f019ec0929c6ffb6e0bdd7e4b5cb
Khaled Emara <mail@KhaledEmara.dev>
2019-12-25 22:59:25 +0200

    implement prim's algorithm and refactor dijkstra

--- prim.cpp
+++ prim.cpp
@@ -2,63 +2,81 @@
 
 using namespace std;
 
-int64_t dijkstra_pq(vector<list<pair<int64_t, uint64_t>>> &graph,
-                    int64_t sourceVertex, int64_t targetVertex) {
+vector<pair<int64_t, int64_t>>
+prim_pq(vector<list<pair<int64_t, uint64_t>>> &graph) {
   vector<int64_t> min_distance(graph.size(), numeric_limits<int64_t>::max());
-  min_distance[sourceVertex] = 0;
+  min_distance[0] = 0;
 
   priority_queue<pair<int64_t, int64_t>, vector<pair<int64_t, int64_t>>,
                  greater<pair<int64_t, int64_t>>>
       active_vertices;
-  active_vertices.push({0, sourceVertex});
+  active_vertices.push({0, 0});
+
+  vector<bool> spanned(graph.size(), false);
+
+  vector<int64_t> parent(graph.size(), 0);
+
+  vector<pair<int64_t, int64_t>> mst_edges;
 
   while (!active_vertices.empty()) {
     int source = active_vertices.top().second;
     active_vertices.pop();
 
-    if (source == targetVertex)
-      return min_distance[source];
+    spanned[source] = true;
+
+    if (source != 0)
+      mst_edges.push_back({parent[source], source});
 
     for (auto edge : graph[source]) {
       auto destination = edge.first;
       auto weight = edge.second;
 
-      if (min_distance[destination] > min_distance[source] + weight) {
-        min_distance[destination] = min_distance[source] + weight;
+      if (!spanned[destination] && min_distance[destination] > weight) {
+        min_distance[destination] = weight;
         active_vertices.push({min_distance[destination], destination});
+        parent[destination] = source;
       }
     }
   }
-  return numeric_limits<int64_t>::max();
+  return mst_edges;
 }
 
-int64_t dijkstra_set(vector<list<pair<int64_t, uint64_t>>> &graph,
-                     int64_t sourceVertex, int64_t targetVertex) {
+vector<pair<int64_t, int64_t>>
+prim_set(vector<list<pair<int64_t, uint64_t>>> &graph) {
   vector<int64_t> min_distance(graph.size(), numeric_limits<int64_t>::max());
-  min_distance[sourceVertex] = 0;
+  min_distance[0] = 0;
 
   set<pair<int64_t, int64_t>> active_vertices;
-  active_vertices.insert({0, sourceVertex});
+  active_vertices.insert({0, 0});
+
+  vector<bool> spanned(graph.size(), false);
+
+  vector<int64_t> parent(graph.size(), -1);
+
+  vector<pair<int64_t, int64_t>> mst_edges;
 
   while (!active_vertices.empty()) {
     int source = active_vertices.begin()->second;
     active_vertices.erase(active_vertices.begin());
 
-    if (source == targetVertex)
-      return min_distance[source];
+    spanned[source] = true;
+
+    if (source != 0)
+      mst_edges.push_back({parent[source], source});
 
     for (auto edge : graph[source]) {
       auto destination = edge.first;
       auto weight = edge.second;
 
-      if (min_distance[destination] > min_distance[source] + weight) {
+      if (!spanned[destination] && min_distance[destination] > weight) {
         active_vertices.erase({min_distance[destination], destination});
-        min_distance[destination] = min_distance[source] + weight;
+        min_distance[destination] = weight;
         active_vertices.insert({min_distance[destination], destination});
+        parent[destination] = source;
       }
     }
   }
-  return numeric_limits<int64_t>::max();
+  return mst_edges;
 }
 
 void addEdge(vector<list<pair<int64_t, uint64_t>>> &graph, int64_t sourceVertex,
@@ -83,6 +101,10 @@ int main() {
   addEdge(graph, 6, 7, 1);
   addEdge(graph, 6, 8, 6);
   addEdge(graph, 7, 8, 7);
-  cout << dijkstra_pq(graph, 0, 8) << endl;
+
+  for (auto edge : prim_pq(graph)) {
+    cout << edge.first << " -> " << edge.second << "\n";
+  }
+
   return EXIT_SUCCESS;
 }
